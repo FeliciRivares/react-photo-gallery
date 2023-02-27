aa0d6dcd0ea11e364ae499face51cb46a23bae28
Karina Ostrovska <karina.ostrovsika@gmail.com>
2023-02-28 01:43:06 +0200

    created requests to the server, configured pagination

--- src/App.js
+++ src/App.js
@@ -1,49 +1,76 @@
 import React from 'react';
 import './index.scss';
+import { Collection } from './Collection';
 
-function Collection({ name, images }) {
-  return (
-    <div className="collection">
-      <img className="collection__big" src={images[0]} alt="Item" />
-      <div className="collection__bottom">
-        <img className="collection__mini" src={images[1]} alt="Item" />
-        <img className="collection__mini" src={images[2]} alt="Item" />
-        <img className="collection__mini" src={images[3]} alt="Item" />
-      </div>
-      <h4>{name}</h4>
-    </div>
-  );
-}
+const categories = [
+   { "name": "All" },
+   { "name": "Sea" },
+   { "name": "Mountains" },
+   { "name": "Architecture" },
+   { "name": "City"}
+]
 
 function App() {
+  const [categoryId, setCategoryId] = React.useState(0);
+  const [page, setPage] = React.useState(1)
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [searchValue, setSearchValue] = React.useState('');
+  const [collection, setCollection] = React.useState([]);
+
+  React.useEffect(() => {
+    setIsLoading(true);
+
+    const cateId = categoryId ? `category=${categoryId}` : '';
+  
+    fetch(`https://63fd11bd859df29986ca1150.mockapi.io/gallery?page=${page}&limit=3&${cateId}`)
+    .then((res) => res.json())
+    .then((json) => {
+      setCollection(json);
+    })
+    .catch((err) => {
+      console.warn(err);
+      alert('Error receiving data');
+    })
+    .finally(() => setIsLoading(false));
+  }, [categoryId, page]);
+
   return (
     <div className="App">
-      <h1>Моя коллекция фотографий</h1>
+      <h1>My photo collection</h1>
       <div className="top">
         <ul className="tags">
-          <li className="active">Все</li>
-          <li>Горы</li>
-          <li>Море</li>
-          <li>Архитектура</li>
-          <li>Города</li>
+          {
+            categories.map((obj, i) => 
+            <li onClick={() => setCategoryId(i)}
+               className={categoryId === i ? 'active' : ''} 
+               key={obj.name}>{obj.name}</li>
+            )
+          }
         </ul>
-        <input className="search-input" placeholder="Поиск по названию" />
+        <input  value={searchValue}
+                onChange={(e) => setSearchValue(e.target.value)}
+                className="search-input"
+                placeholder="Search by name" />
       </div>
       <div className="content">
-        <Collection
-          name="Путешествие по миру"
-          images={[
-            'https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1560840067-ddcaeb7831d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1531219572328-a0171b4448a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1573108724029-4c46571d6490?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-          ]}
-        />
+        {
+          isLoading ? (<h2>Loading...</h2>
+          ) : (
+            collection.filter(obj => {
+              return obj.name.toLowerCase().includes(searchValue.toLowerCase());
+            })
+            .map((obj, index) => (
+              <Collection key={index} name={obj.name} photos={obj.photos}/>
+            ))
+          )
+        }
       </div>
       <ul className="pagination">
-        <li>1</li>
-        <li className="active">2</li>
-        <li>3</li>
+      {
+        [...Array(5)].map((_, i) => 
+            <li onClick={() => setPage(i + 1)} 
+            className={page === i + 1 ? 'active' : ''}>{i + 1}</li>)
+      }
       </ul>
     </div>
   );
